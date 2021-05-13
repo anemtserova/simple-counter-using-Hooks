9f9d01ed4e622ea45d83161e405d8f0bafb51993
Antoniya <anemtserova@gmail.com>
2021-05-13 00:58:57 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,39 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Home(props) {
+	const [userNum, setUserNum] = useState(null);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex">
+			<button
+				onClick={() => {
+					props.myFunction(userNum);
+				}}>
+				Set Number
+			</button>
+			<input
+				type="number"
+				placeholder="Enter a number"
+				onChange={e => {
+					setUserNum(e.target.value);
+				}}></input>
+			<div>{props.number1}</div>
+			<div>{props.number2}</div>
+			<div>{props.number3}</div>
+			<div>{props.number4}</div>
+			<div>{props.number5}</div>
+			<div>{props.number6}</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	number1: PropTypes.string,
+	number2: PropTypes.string,
+	number3: PropTypes.string,
+	number4: PropTypes.string,
+	number5: PropTypes.string,
+	number6: PropTypes.string,
+	myFunction: PropTypes.func
+};
